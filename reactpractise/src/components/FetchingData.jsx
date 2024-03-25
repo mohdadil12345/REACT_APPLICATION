d1eca2e480b3dd5f5cc6e4e7ce2baaac1781d289
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-25 17:02:46 +0530

    filter by category

--- reactpractise/src/components/FetchingData.jsx
+++ reactpractise/src/components/FetchingData.jsx
@@ -1,75 +1,85 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function FetchingData() {
-
   const api = `https://fakestoreapi.com/products`;
 
+  const [prod, setprod] = useState([]);
+  const [globalData, setglobalData] = useState([]);
 
-  const [prod, setprod] = useState([])
-  const [inputval, setinputval] = useState("")
-
-
-  const fetchData = async() => {
-
-     try {
-
-      let res  = await fetch(api)
-      let data = await res.json()
-      console.log(data)
-      setprod(data)
-      
-     } catch (error) {
-      console.log(error)
-     }
-
-  }
+  const fetchData = async () => {
+    try {
+      let res = await fetch(api);
+      let data = await res.json();
+      console.log(data);
+      setprod(data);
+      setglobalData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
-  
+    fetchData();
+  }, []);
 
+  //  search by title
 
-//  search by title
+  const serchfilter = (e) => {
+    // console.log(e.target.value)
+    let filter_search = globalData.filter((ele) =>
+    ele.title.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  setprod(filter_search);  
+  }
 
-const searchdata = prod.filter(ele => ele.title.toLowerCase().includes(inputval.toLowerCase()))
+  //  filter by category
+  const handle_category = (e) => {
+     console.log(e.target.value)
 
+    if(e.target.value == "") {
+       setprod(globalData)
+    }else{
+       let fil_cat = globalData.filter((ele) => 
+          ele.category.includes(e.target.value)
+       )
+       setprod(fil_cat)
+    }
 
-
+  }
 
 
   return (
     <>
-         <input onChange={(e) => setinputval(e.target.value)}  className='searc_input' type="text" placeholder='search by title' />
+      <input
+           onChange={(e) => serchfilter(e)}
+        className="searc_input"
+        type="text"
+        placeholder="search by title"
+      />
 
+      <select onChange={(e) => handle_category(e)} className="select_tag" name="category">
+        <option value="">Select By Category</option>
+        <option value="men's clothing">Mens</option>
+        <option value="women's clothing">Womens</option>
+        <option value="electronics">Electronics</option>
+        <option value="jewelery">Jewellery</option>
+      </select>
 
-   <select className='select_tag' name="category">
-      <option value="mens">Mens</option>
-      <option value="womens">Womens</option>
-      <option value="electronics">Electronics</option>
-      <option value="jewellery">Jewellery</option>
-   </select>
-
-    <div className='prod_cont'>
-         
-
-      {searchdata.map((ele) => (
+      <div className="prod_cont">
+        {prod.map((ele) => (
           <div className="prod_items">
-             <img src={ele.image} alt="" />
-             <h4>{ele.title}</h4>
-             <p>{ele.category}</p>
-             <p>{ele.price}</p>
+            <img src={ele.image} alt="" />
+            <h4>{ele.title}</h4>
+            <p>{ele.category}</p>
+            <p>{ele.price}</p>
           </div>
-      ))}
-
-    </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default FetchingData
-
-
+export default FetchingData;
 
 // const deleteClick = async (id) => {
 //   try {
@@ -89,8 +99,6 @@ export default FetchingData
 //   } catch (error) {}
 // };
 
-
-
 // try {
 //   const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR', {
 //     method: 'POST',
@@ -104,7 +112,6 @@ export default FetchingData
 
 //   const data = await response.json();
 //   console.log(data);
-
 
 // } catch (error) {
 
