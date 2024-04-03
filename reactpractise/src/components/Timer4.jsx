b41097e7e759e38a200d04f8ae64c39b178e8e72
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-04 04:07:16 +0530

    timer

--- reactpractise/src/components/Timer4.jsx
+++ reactpractise/src/components/Timer4.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Timer4() {
 
-    const [currTime, setcurrTime] = useState(0)
+    const [currTime, setcurrTime] = useState(10)
+
+
+
+
+    let id;
+
+  useEffect(() => {
+
+
+     const timer = () => {
+        id = setInterval(() => {
+
+           setcurrTime((prev)=> {
+             if(currTime <=0 ){
+              return 0
+             }
+             return prev - 1
+           })
+          
+        }, 1000);
+
+
+     }
+
+     timer()
+     return ()=> {clearInterval(id)}
+
+ 
+  }, [currTime])
+  
+
+
 
   return (
     <div>
 
-    <input type="text" placeholder='time in seconds' />
 
     <p>Timer4 : {currTime} </p>
 
