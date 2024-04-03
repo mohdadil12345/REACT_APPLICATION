c5455bce82ba5ad903e176a0c97a94656c7abbae
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-04 04:07:09 +0530

    edit functionality

--- reactpractise/src/components/Todolist.jsx
+++ reactpractise/src/components/Todolist.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 function Todolist() {
   const [title, settile] = useState("");
   const [todo, settodo] = useState([]);
+  const [editID, seteditID] = useState(null);
+  const [editTitle, setEditTitle] = useState("");
 
   const handle_add = () => {
     console.log(title);
@@ -11,10 +13,15 @@ function Todolist() {
       title: title,
       id: Date.now(),
       status: "completed",
+
     };
     // console.log(newtodo)
     settodo([...todo, newtodo]);
+    settile("")
+
   };
+
+  console.log(todo)
 
   useEffect(() => {
     const lsdata = JSON.parse(localStorage.getItem("todos"));
@@ -23,9 +30,16 @@ function Todolist() {
     }
   }, []);
 
+
+
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todo));
   }, [todo]);
+
+
+
+
+
 
   //  delete
   const handle_delete = (id) => {
@@ -49,6 +63,30 @@ function Todolist() {
 
   };
 
+
+
+  const handle_edit = (item) => {
+       console.log(item)
+
+       seteditID(item.id)
+       setEditTitle(item.title)
+      
+
+  }
+
+
+
+
+  const handleSaveEdit = (id) => {
+    let editedTodo = todo.map((ele) =>
+      ele.id === id ? { ...ele, title: editTitle } : ele
+    );
+    settodo(editedTodo);
+    seteditID(null);
+  };
+
+
+
   return (
     <div className="todolist">
       <input
@@ -61,11 +99,30 @@ function Todolist() {
       <div className="todo">
         {todo.map((ele) => (
           <div key={ele.id} className="todoitems">
-            <h2>Title : {ele.title}</h2>
-            <p>Status : {ele.status ? "completed" : "not completed"}</p>
+
+            
+      {editID == ele.id ?    <div>
+                <input
+                  type="text"
+                  value={editTitle}
+                  onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <button onClick={() => handleSaveEdit(ele.id)}>Save</button>
+              </div>  :      <div>
+       <h2>Title : {ele.title}</h2>
+            <p>Status : <span  style={{color : ele.status ? "green" : "red"}} >{ele.status ? "completed" : "not completed"}</span></p>
             <button onClick={() => handle_delete(ele.id)}>DELETE</button>
             <button onClick={()=> handle_toggle(ele.id)}>TOGGLE</button>
+            <button onClick={()=> handle_edit(ele)}>EDIT</button>
+
+       </div>
+
+}
+
+  
           </div>
+
+
         ))}
       </div>
     </div>
