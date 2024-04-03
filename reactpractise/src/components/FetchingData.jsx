cb3724544c6ec3d281ea811ee121728b6c93bd6f
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-04 04:06:53 +0530

    sorting functionality

--- reactpractise/src/components/FetchingData.jsx
+++ reactpractise/src/components/FetchingData.jsx
@@ -1,10 +1,62 @@
 import React, { useEffect, useState } from "react";
 
+
+const initialdata = {
+  title : "",
+  image : "",
+  price : "",
+  description : "",
+  categoryy : ""
+}
+
+
+
 function FetchingData() {
   const api = `https://fakestoreapi.com/products`;
 
   const [prod, setprod] = useState([]);
   const [globalData, setglobalData] = useState([]);
+  const [data, setdata] = useState(initialdata)
+
+const handle_chnagee = (e) => {
+    const {name, value} = e.target
+    // console.log(name, value)
+
+      let obj = {...prod}
+      obj[name] = value
+
+      // console.log("obj", obj)
+      setdata(obj)
+
+}
+
+
+const handle_form = async(e) => {
+  e.preventDefault()
+     console.log("data", data)
+
+   try {
+    let res  = await fetch("https://fakestoreapi.com/products", {
+        method : "POST",
+        headers : {"Content-Type" : "application/json"}, 
+        body : JSON.stringify(data)
+    }) 
+
+    let postdata = await res.json()
+    alert("data added")
+    console.log("postdata", postdata)
+    // setprod([...prod, postdata]);
+    // fetchData([...prod, postdata])
+
+    
+   } catch (error) {
+     console.log(error)
+   }
+
+
+}
+
+
 
   const fetchData = async () => {
     try {
@@ -48,8 +100,49 @@ function FetchingData() {
   }
 
 
+  // sorting
+  const handle_sorting = (e) => {
+        // console.log(e.target.value)
+
+    if(e.target.value == "asc") {
+        let sort_asc = [...globalData].sort((a,b)=> a.price - b.price)
+        console.log("asc", sort_asc)
+        setprod(sort_asc)
+    } else if (e.target.value == "desc"){
+      let sort_descsc = [...globalData].sort((a,b)=> b.price - a.price)
+      console.log("desc", sort_descsc)
+
+      setprod(sort_descsc)
+    }else{
+      setprod(globalData)
+    }
+    
+
+  }
+
+
   return (
     <>
+
+
+     <form onSubmit={(e)=> handle_form(e)}  className="postada">
+        <input onChange={(e)=>handle_chnagee(e)} name="title" type="text" placeholder="title" />
+        <input onChange={(e)=>handle_chnagee(e)}  name="image" type="text" placeholder="image"/>
+        <input onChange={(e)=>handle_chnagee(e)}  name="price" type="number" placeholder="price" />
+        <input onChange={(e)=>handle_chnagee(e)}  name="description" type="text" placeholder="description" />
+         <select onChange={(e)=>handle_chnagee(e)}  name="categoryy">
+         <option value="">Select category</option>
+         <option value="men's clothing">Mens</option>
+        <option value="women's clothing">Womens</option>
+        <option value="electronics">Electronics</option>
+        <option value="jewelery">Jewellery</option>
+         </select>
+       
+       <button type="submit">ADD</button>
+
+     </form>
+
+
       <input
            onChange={(e) => serchfilter(e)}
         className="searc_input"
@@ -63,6 +156,14 @@ function FetchingData() {
         <option value="women's clothing">Womens</option>
         <option value="electronics">Electronics</option>
         <option value="jewelery">Jewellery</option>
+      </select>
+
+      {/*  sorting */}
+
+      <select onChange={(e)=> handle_sorting(e)} name="sorting">
+        <option value="">sort by price</option>
+        <option value="asc">ASC</option>
+        <option value="desc">DESC</option>
       </select>
 
       <div className="prod_cont">
@@ -99,20 +200,3 @@ export default FetchingData;
 //   } catch (error) {}
 // };
 
-// try {
-//   const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       // Include any request body parameters here
-//     })
-//   });
-
-//   const data = await response.json();
-//   console.log(data);
-
-// } catch (error) {
-
-// }
