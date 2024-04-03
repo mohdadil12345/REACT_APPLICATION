ded73be72e86ff10cea1aacf744cb1cc5b0c7487
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-04 04:06:25 +0530

    calculator with two input

--- reactpractise/src/components/Calculator.jsx
+++ reactpractise/src/components/Calculator.jsx
@@ -1,49 +1,55 @@
 import React, { useState } from "react";
+import Calculator2 from "./Calculator2";
 
 function Calculator() {
   const [val1, setval1] = useState("");
   const [val2, setval2] = useState("");
   const [ans, setans] = useState("");
 
-
-
-
   const handle_add = () => {
-
-       setans (parseInt(val1) + parseInt(val2))
-  }
+    setans(parseInt(val1) + parseInt(val2));
+  };
 
   const hand_sub = () => {
-    setans (parseInt(val1) - parseInt(val2))
-  }
+    setans(parseInt(val1) - parseInt(val2));
+  };
   const hand_mult = () => {
-    setans (parseInt(val1) * parseInt(val2))
-  }
+    setans(parseInt(val1) * parseInt(val2));
+  };
   const hand_div = () => {
-    setans (parseInt(val1) / parseInt(val2))
-
-  }
-
-
+    setans(parseInt(val1) / parseInt(val2));
+  };
 
   return (
-    <div className="calculator">
+    <>
+      <div className="calculator">
+        <input
+          onChange={(e) => setval1(e.target.value)}
+          type="number"
+          placeholder="calculate"
+        />
 
-      <input onChange={(e)=> setval1(e.target.value)}  type="number" placeholder="calculate" />
+        <input
+          onChange={(e) => setval2(e.target.value)}
+          type="number"
+          placeholder="calculate"
+        />
 
-      <input onChange={(e)=> setval2(e.target.value)} type="number" placeholder="calculate" />
+        <h3>ans : {ans}</h3>
 
-      <h3>ans : {ans}</h3>
-
-  
-
-      <div className="btnn">
-        <button onClick={handle_add}>ADD</button>
-        <button onClick={hand_sub}>SUBTRACT</button>
-        <button onClick={hand_mult}>MULTIPLY</button>
-        <button onClick={hand_div}>DIVIDE</button>
+        <div className="btnn">
+          <button onClick={handle_add}>ADD</button>
+          <button onClick={hand_sub}>SUBTRACT</button>
+          <button onClick={hand_mult}>MULTIPLY</button>
+          <button onClick={hand_div}>DIVIDE</button>
+        </div>
       </div>
-    </div>
+
+      <hr />
+      <hr />
+
+      <Calculator2 />
+    </>
   );
 }
 
