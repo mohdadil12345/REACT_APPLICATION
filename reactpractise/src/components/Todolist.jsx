29def9e4fcc6b322c275dd5f6eb97c882c45274f
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-13 13:03:12 +0530

    css design for todo

--- reactpractise/src/components/Todolist.jsx
+++ reactpractise/src/components/Todolist.jsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 function Todolist() {
   const [title, settile] = useState("");
   const [todo, settodo] = useState([]);
-  const [editID, seteditID] = useState(null);
-  const [editTitle, setEditTitle] = useState("");
+
+
 
   const handle_add = () => {
     console.log(title);
@@ -13,15 +13,13 @@ function Todolist() {
       title: title,
       id: Date.now(),
       status: "completed",
-
     };
     // console.log(newtodo)
     settodo([...todo, newtodo]);
-    settile("")
-
+    settile("");
   };
 
-  console.log(todo)
+  console.log(todo);
 
   useEffect(() => {
     const lsdata = JSON.parse(localStorage.getItem("todos"));
@@ -30,16 +28,9 @@ function Todolist() {
     }
   }, []);
 
-
-
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todo));
   }, [todo]);
-
-
-
-
-
 
   //  delete
   const handle_delete = (id) => {
@@ -48,81 +39,57 @@ function Todolist() {
     settodo(del_item);
   };
 
-
   //  toggle
   const handle_toggle = (id) => {
-     
-    let tog_item = todo.map((ele)=> (
-        ele.id == id ? {
-         ...ele,   status : !ele.status
-        } : ele
-    ))
+    let tog_item = todo.map((ele) =>
+      ele.id == id
+        ? {
+            ...ele,
+            status: !ele.status,
+          }
+        : ele
+    );
 
-    settodo(tog_item)
-
-
+    settodo(tog_item);
   };
 
-
-
   const handle_edit = (item) => {
-       console.log(item)
+    console.log(item);
 
-       seteditID(item.id)
-       setEditTitle(item.title)
-      
-
-  }
-
-
-
-
-  const handleSaveEdit = (id) => {
-    let editedTodo = todo.map((ele) =>
-      ele.id === id ? { ...ele, title: editTitle } : ele
-    );
-    settodo(editedTodo);
-    seteditID(null);
   };
 
 
 
   return (
     <div className="todolist">
-      <input
+    
+    <div className="info">
+    <input
         onChange={(e) => settile(e.target.value)}
         type="text"
         placeholder="add todo"
       />
       <button onClick={handle_add}>ADD</button>
+    </div>
 
       <div className="todo">
         {todo.map((ele) => (
           <div key={ele.id} className="todoitems">
 
-            
-      {editID == ele.id ?    <div>
-                <input
-                  type="text"
-                  value={editTitle}
-                  onChange={(e) => setEditTitle(e.target.value)}
-                />
-                <button onClick={() => handleSaveEdit(ele.id)}>Save</button>
-              </div>  :      <div>
-       <h2>Title : {ele.title}</h2>
-            <p>Status : <span  style={{color : ele.status ? "green" : "red"}} >{ele.status ? "completed" : "not completed"}</span></p>
+              <h2>Title : {ele.title}</h2>
+              <p>
+                Status :{" "}
+                <span style={{ color: ele.status ? "green" : "red" }}>
+                  {ele.status ? "completed" : "not completed"}
+                </span>
+              </p>
+            <div className="btns">
             <button onClick={() => handle_delete(ele.id)}>DELETE</button>
-            <button onClick={()=> handle_toggle(ele.id)}>TOGGLE</button>
-            <button onClick={()=> handle_edit(ele)}>EDIT</button>
-
-       </div>
-
-}
-
-  
+              <button onClick={() => handle_toggle(ele.id)}>TOGGLE</button>
+              <button onClick={() => handle_edit(ele)}>EDIT</button>
+            </div>
+     
           </div>
-
-
         ))}
       </div>
     </div>
