4c1ea33486b7b355cfb6aa5babc4c133de15125a
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-17 12:54:34 +0530

    Update Todolist.jsx

--- reactpractise/src/components/Todolist.jsx
+++ reactpractise/src/components/Todolist.jsx
@@ -4,8 +4,6 @@ function Todolist() {
   const [title, settile] = useState("");
   const [todo, settodo] = useState([]);
 
-
-
   const handle_add = () => {
     console.log(title);
 
@@ -41,54 +39,41 @@ function Todolist() {
 
   //  toggle
   const handle_toggle = (id) => {
-    let tog_item = todo.map((ele) =>
-      ele.id == id
-        ? {
-            ...ele,
-            status: !ele.status,
-          }
-        : ele
-    );
+    let tog_item = todo.map((ele) => ele.id == id ? {...ele, status: !ele.status,}: ele);
 
     settodo(tog_item);
   };
 
   const handle_edit = (item) => {
     console.log(item);
-
   };
-
-
 
   return (
     <div className="todolist">
-    
-    <div className="info">
-    <input
-        onChange={(e) => settile(e.target.value)}
-        type="text"
-        placeholder="add todo"
-      />
-      <button onClick={handle_add}>ADD</button>
-    </div>
+      <div className="info">
+        <input
+          onChange={(e) => settile(e.target.value)}
+          type="text"
+          placeholder="add todo"
+        />
+        <button onClick={handle_add}>ADD</button>
+      </div>
 
       <div className="todo">
         {todo.map((ele) => (
           <div key={ele.id} className="todoitems">
-
-              <h2>Title : {ele.title}</h2>
-              <p>
-                Status :{" "}
-                <span style={{ color: ele.status ? "green" : "red" }}>
-                  {ele.status ? "completed" : "not completed"}
-                </span>
-              </p>
+            <h2>Title : {ele.title}</h2>
+            <p>
+              Status :{" "}
+              <span style={{ color: ele.status ? "green" : "red" }}>
+                {ele.status ? "completed" : "not completed"}
+              </span>
+            </p>
             <div className="btns">
-            <button onClick={() => handle_delete(ele.id)}>DELETE</button>
+              <button onClick={() => handle_delete(ele.id)}>DELETE</button>
               <button onClick={() => handle_toggle(ele.id)}>TOGGLE</button>
               <button onClick={() => handle_edit(ele)}>EDIT</button>
             </div>
-     
           </div>
         ))}
       </div>
